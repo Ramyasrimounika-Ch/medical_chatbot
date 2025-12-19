8ff0553e728aef245678cfb1268e03426ce9907f
Ramyasrimounika-Ch <ramyasrimounika03@gmail.com>
2025-12-19 19:37:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, jsonify, request
+from store_index import load_data
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+from src.prompt import *
+import os
+import pickle
+app = Flask(__name__)
+file_path="vector.pkl"
+
+if not os.path.exists(file_path):
+    load_data()
+
+with open(file_path,"rb") as f:
+    db=pickle.load(f)
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs={"prompt": PROMPT}
+
+llm = CTransformers(
+    model="TheBloke/Llama-2-7B-Chat-GGML",
+    model_file="llama-2-7b-chat.ggmlv3.q4_0.bin",
+    model_type="llama",
+    config={"max_new_tokens": 512, "temperature": 0.8}
+)
+
+
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=db.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True, 
+    chain_type_kwargs=chain_type_kwargs)
+
+
+#this is default route it will be working like homepage
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
+
